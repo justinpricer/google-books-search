ec9fa2f0f1abbb486dc93527b26c3502daa3428b
Justin Pricer <justin.pricer@gmail.com>
2020-12-15 19:18:46 -0600

    run build

---
+++ client/build/precache-manifest.76f4576def55c4a2e0c0e7b5dc5c2771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6db65bc3bbf71f87ed03",
+    "url": "/static/css/main.d6e2cd86.chunk.css"
+  },
+  {
+    "revision": "6db65bc3bbf71f87ed03",
+    "url": "/static/js/main.a4753eea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "193b767e333ca6393c61",
+    "url": "/static/js/2.3bb06115.chunk.js"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/static/media/blur-book-stack-books-bookshelves-590493.d8631c5e.jpg"
+  },
+  {
+    "revision": "0802b55c45d586a0acf16c7b20545017",
+    "url": "/index.html"
+  }
+];
